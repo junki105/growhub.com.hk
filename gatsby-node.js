4143bb9a615c960db337caed33e8916c14ab7489
junki105 <junki.dev@gmail.com>
2019-08-30 12:44:09 +0800

    refactor: added _redirects settings.

--- gatsby-node.js
+++ gatsby-node.js
@@ -10,12 +10,10 @@ const { createFilePath } = require(`gatsby-source-filesystem`)
 
 exports.createPages = ({ actions }) => {
   const { createRedirect } = actions
-
-  // Netlifyの_redirectsファイルを生成
-  // createRedirect({
-  //   fromPath: 'https://growhub.netlify.com/*',
-  //   toPath: 'https://growhub.com.hk/:splat',
-  //   isPermanent: true,
-  //   force: true
-  // })
+  createRedirect({
+    fromPath: 'https://growhub.netlify.com/*',
+    toPath: 'https://www.growhub.com.hk/:splat',
+    isPermanent: true,
+    force: true
+  })
 }
